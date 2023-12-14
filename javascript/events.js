6a2aaa8d4576a599d3f8c1c339b49247cd405f53
Neel Kachhia <neel.kachhia13@gmail.com>
2023-12-13 23:55:22 -0500

    like dislike done in all

--- javascript/events.js
+++ javascript/events.js
@@ -31,14 +31,128 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("permission already granted!");
   }
 
-  const likeButton = document.getElementById("thumsup");
-  const dislikeButton = document.getElementById("thumbsdown");
-  const likeIcon = document.getElementById("likeIcon");
-  const dislikeIcon = document.getElementById("dislikeIcon");
-  const likedIcon = document.getElementById("likedIcon");
-  const dislikedIcon = document.getElementById("dislikedIcon");
-  const likeCount = document.getElementById("likeCount");
-  const dislikeCount = document.getElementById("dislikeCount");
+  changeLikeDislike(1);
+  changeLikeDislike(2);
+  changeLikeDislike(3);
+  changeLikeDislike(4);
+  changeLikeDislike(5);
+  changeLikeDislike(6);
+  // const likeButton = document.getElementById("thumsup");
+  // const dislikeButton = document.getElementById("thumbsdown");
+  // const likeIcon = document.getElementById("likeIcon");
+  // const dislikeIcon = document.getElementById("dislikeIcon");
+  // const likedIcon = document.getElementById("likedIcon");
+  // const dislikedIcon = document.getElementById("dislikedIcon");
+  // const likeCount = document.getElementById("likeCount");
+  // const dislikeCount = document.getElementById("dislikeCount");
+
+  // // Set initial state
+  // let initialLikeCount = Math.floor(Math.random() * 100);
+  // let initialDislikeCount = Math.floor(Math.random() * 100);
+
+  // likeCount.textContent = initialLikeCount;
+  // dislikeCount.textContent = initialDislikeCount;
+  // likeIcon.style.display = "inline-block";
+  // dislikeIcon.style.display = "inline-block";
+  // // Event listeners for like and dislike buttons
+  // likeButton.addEventListener("click", function () {
+  //   if (
+  //     likeIcon.style.display === "inline-block" &&
+  //     dislikeIcon.style.display === "inline-block"
+  //   ) {
+  //     likedIcon.style.display = "inline-block";
+  //     likeIcon.style.display = "none";
+  //     likeCount.textContent = initialLikeCount + 1;
+  //   } else if (likedIcon.style.display === "inline-block") {
+  //     likedIcon.style.display = "none";
+  //     likeIcon.style.display = "inline-block";
+  //     likeCount.textContent = initialLikeCount;
+  //   } else {
+  //     likedIcon.style.display = "inline-block";
+  //     likeIcon.style.display = "none";
+  //     likeCount.textContent = initialLikeCount + 1;
+  //     dislikedIcon.style.display = "none";
+  //     dislikeIcon.style.display = "inline-block";
+  //     dislikeCount.textContent = initialDislikeCount;
+  //   }
+  // });
+  // dislikeButton.addEventListener("click", function () {
+  //   if (
+  //     likeIcon.style.display === "inline-block" &&
+  //     dislikeIcon.style.display === "inline-block"
+  //   ) {
+  //     dislikedIcon.style.display = "inline-block";
+  //     dislikeIcon.style.display = "none";
+  //     dislikeCount.textContent = initialDislikeCount + 1;
+  //   } else if (dislikedIcon.style.display === "inline-block") {
+  //     dislikedIcon.style.display = "none";
+  //     dislikeIcon.style.display = "inline-block";
+  //     dislikeCount.textContent = initialDislikeCount;
+  //   } else {
+  //     likedIcon.style.display = "none";
+  //     likeIcon.style.display = "inline-block";
+  //     likeCount.textContent = initialLikeCount;
+  //     dislikedIcon.style.display = "inline-block";
+  //     dislikeIcon.style.display = "none";
+  //     dislikeCount.textContent = initialDislikeCount + 1;
+  //   }
+  // });
+
+  const hackathonDate = new Date("2023-12-17T00:00:00");
+
+  // Function to update the countdown
+  function updateCountdown() {
+    const now = new Date();
+    const timeDifference = hackathonDate - now;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("customDays").innerText = days;
+    document.getElementById("customHours").innerText = hours;
+    document.getElementById("customMinutes").innerText = minutes;
+    document.getElementById("customSeconds").innerText = seconds;
+  }
+
+  // Update the countdown every second
+  setInterval(updateCountdown, 1000);
+
+  // Initial update
+  updateCountdown();
+});
+
+// let notification, interval;
+// document.addEventListener("visibilitychange", () => {
+//   if (document.visibilityState === "hidden") {
+//     let leaveDate = new Date();
+//     interval = setInterval(() => {
+//       notification = new Notification("Come back please!!!", {
+//         body: `You have been away since last ${Math.round(
+//           (new Date() - leaveDate) / 1000
+//         )} seconds.`,
+//         tag: "Come back",
+//       });
+//     }, 5000);
+//   } else {
+//     if (interval) clearInterval(interval);
+//     if (notification) notification.close();
+//   }
+// });
+function changeLikeDislike(EventId) {
+  const likeButton = document.getElementById("thumsup" + EventId);
+  const dislikeButton = document.getElementById("thumbsdown" + EventId);
+  const likeIcon = document.getElementById("likeIcon" + EventId);
+  const dislikeIcon = document.getElementById("dislikeIcon" + EventId);
+  const likedIcon = document.getElementById("likedIcon" + EventId);
+  const dislikedIcon = document.getElementById("dislikedIcon" + EventId);
+  const likeCount = document.getElementById("likeCount" + EventId);
+  const dislikeCount = document.getElementById("dislikeCount" + EventId);
 
   // Set initial state
   let initialLikeCount = Math.floor(Math.random() * 100);
@@ -91,54 +205,7 @@ document.addEventListener("DOMContentLoaded", function () {
       dislikeCount.textContent = initialDislikeCount + 1;
     }
   });
-
-  const hackathonDate = new Date("2023-12-17T00:00:00");
-
-  // Function to update the countdown
-  function updateCountdown() {
-    const now = new Date();
-    const timeDifference = hackathonDate - now;
-
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor(
-      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-    document.getElementById("customDays").innerText = days;
-    document.getElementById("customHours").innerText = hours;
-    document.getElementById("customMinutes").innerText = minutes;
-    document.getElementById("customSeconds").innerText = seconds;
-  }
-
-  // Update the countdown every second
-  setInterval(updateCountdown, 1000);
-
-  // Initial update
-  updateCountdown();
-});
-
-// let notification, interval;
-// document.addEventListener("visibilitychange", () => {
-//   if (document.visibilityState === "hidden") {
-//     let leaveDate = new Date();
-//     interval = setInterval(() => {
-//       notification = new Notification("Come back please!!!", {
-//         body: `You have been away since last ${Math.round(
-//           (new Date() - leaveDate) / 1000
-//         )} seconds.`,
-//         tag: "Come back",
-//       });
-//     }, 5000);
-//   } else {
-//     if (interval) clearInterval(interval);
-//     if (notification) notification.close();
-//   }
-// });
-
+}
 function openPopup(headingText, eventData) {
   let popupContainer = document.getElementById("popupContainer");
   let popupHeading = document.getElementById("popupHeading");
@@ -190,44 +257,3 @@ function closePopup() {
   popupImage.src = "";
   document.body.style.overflow = "auto";
 }
-
-// function toggleLikeDislike(isLike) {
-//   const likeIcon = document.getElementById("likeIcon");
-//   const dislikeIcon = document.getElementById("dislikeIcon");
-//   const likedIcon = document.getElementById("likedIcon");
-//   const dislikedIcon = document.getElementById("dislikedIcon");
-//   const likeCount = document.getElementById("likeCount");
-//   const dislikeCount = document.getElementById("dislikeCount");
-
-//   let currentLikeCount = parseInt(likeCount.textContent);
-//   let currentDislikeCount = parseInt(dislikeCount.textContent);
-
-//   if (isLike) {
-//     if (likeIcon.style.display === "none") {
-//       likedIcon.style.display = "none";
-//       likeIcon.style.display = "inline-block";
-//       likeCount.textContent = currentLikeCount - 1;
-//     } else {
-//       likeIcon.style.display = "none";
-//       likedIcon.style.display = "inline-block";
-//       dislikeIcon.style.display = "none";
-//       dislikedIcon.style.display = "inline-block";
-//       likeCount.textContent = currentLikeCount + 1;
-//       dislikeCount.textContent =
-//         currentDislikeCount > 0 ? currentDislikeCount - 1 : 0;
-//     }
-//   } else {
-//     if (dislikeIcon.style.display === "none") {
-//       dislikedIcon.style.display = "none";
-//       dislikeIcon.style.display = "inline-block";
-//       dislikeCount.textContent = currentDislikeCount - 1;
-//     } else {
-//       dislikeIcon.style.display = "none";
-//       dislikedIcon.style.display = "inline-block";
-//       likeIcon.style.display = "none";
-//       likedIcon.style.display = "inline-block";
-//       dislikeCount.textContent = currentDislikeCount + 1;
-//       likeCount.textContent = currentLikeCount > 0 ? currentLikeCount - 1 : 0;
-//     }
-//   }
-// }
