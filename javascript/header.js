b6d89e15eefa0e9ada755e4e6b459dc4c1562a70
Neel Kachhia <neel.kachhia13@gmail.com>
2023-12-13 23:01:15 -0500

    Update header.js

--- javascript/header.js
+++ javascript/header.js
@@ -6,6 +6,12 @@ menuItems.forEach(function (item) {
   var pageName = item.getAttribute("href").split("/").pop().split(".")[0];
 
   if (currentPage.includes(pageName)) {
-    item.classList.add("active");
+    if (pageName == "") {
+    } else {
+      item.classList.add("active");
+    }
+    if (pageName == currentPage) {
+      item.classList.add("active");
+    }
   }
 });
